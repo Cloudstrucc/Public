91491974da2675905d077f1250c6cd8316be9cef
Frederick Pearson <frederick.pearson@international.gc.ca>
2024-05-07 02:47:03 -0400

    install function

--- buildwetfromscratch_enhanced.ps1
+++ buildwetfromscratch_enhanced.ps1
@@ -568,17 +568,17 @@ function CreateSampleWeblinkSetWizard {
 
 
 function RunPortalTemplateInstall {
-    # $zipFilePath = "C:\themes-dist-14.1.0-gcweb.zip"
-    # $extractionPath = "C:\Users\Fred\source\repos\pub\Public\files\themes-dist-14.1.0-gcweb" 
+    $zipFilePath = "C:\themes-dist-14.1.0-gcweb.zip"
+    $extractionPath = "C:\Users\Fred\source\repos\pub\Public\files\themes-dist-14.1.0-gcweb" 
     
-    # Expand-Archive -Path $zipFilePath -DestinationPath $extractionPath -Force
-    # Write-Host $extractionPath
-    # WriteHierarchy -path $extractionPath -parentPageId $homePageId
+    Expand-Archive -Path $zipFilePath -DestinationPath $extractionPath -Force
+    Write-Host $extractionPath
+    WriteHierarchy -path $extractionPath -parentPageId $homePageId
         
-    # CreateSnippets
-    # $rootFolderPath = "C:\Users\Fred\source\repos\pub\Public\liquid\webtemplates"
-    # Write-Templates -folderPath $rootFolderPath
-    # UpdateHomePage -pageTemplateName "CS-Home-WET"
+    CreateSnippets
+    $rootFolderPath = "C:\Users\Fred\source\repos\pub\Public\liquid\webtemplates"
+    Write-Templates -folderPath $rootFolderPath
+    UpdateHomePage -pageTemplateName "CS-Home-WET"
     UpdateBaselineStyles
 }
 
