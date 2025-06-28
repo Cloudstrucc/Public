74c82c87f9d829ce0a14a2469e8def7752e6212f
Frederick Pearson <fpearson@cloudstrucc.com>
2025-06-28 06:57:36 -0400

    ecomm

--- apps/aries-canada/scripts/create-bifold-invitation.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -X POST http://localhost:3001/connections/create-invitation -H "Content-Type: application/json" -d '{"alias":"bifold-user","auto_accept":true}'
--- apps/aries-canada/scripts/deploy-sandbox.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-RESOURCE_GROUP=ariesCanadaRG
-az group create --name $RESOURCE_GROUP --location canadacentral
-az deployment group create --resource-group $RESOURCE_GROUP --template-file infra/sandbox-arm/azuredeploy.json --parameters infra/sandbox-arm/azuredeploy.parameters.json
