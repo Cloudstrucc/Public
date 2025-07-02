4f88dba04d616786b17c9405d67942599f2c95f1
Frederick Pearson <fpearson@cloudstrucc.com>
2025-07-01 22:06:04 -0400

    airies

--- apps/airies/scripts/deploy-sandbox.sh
+++ apps/airies/scripts/deploy-sandbox.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 RESOURCE_GROUP=ariesCanadaRG
 LOCATION=canadacentral
-
 az group create --name $RESOURCE_GROUP --location $LOCATION
 az deployment group create \
-  --resource-group $RESOURCE_GROUP \
-  --template-file infra/sandbox-arm/azuredeploy.json \
-  --parameters infra/sandbox-arm/azuredeploy.parameters.json
+--resource-group $RESOURCE_GROUP \
+--template-file infra/sandbox-arm/azuredeploy.json \
+--parameters infra/sandbox-arm/azuredeploy.parameters.json
