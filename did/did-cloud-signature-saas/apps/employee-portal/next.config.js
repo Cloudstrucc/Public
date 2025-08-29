c9802e57cdf25a11e16719da4190ad622c153c0d
Frederick Pearson <fpearson@cloudstrucc.com>
2025-08-29 08:09:27 -0400

    employee portal

--- did/did-cloud-signature-saas/apps/employee-portal/next.config.js
+++ did/did-cloud-signature-saas/apps/employee-portal/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Force Pages Router so pages/ is used (not app/)
   experimental: { appDir: false }
 };
 module.exports = nextConfig;
