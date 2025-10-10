5f9193af2968abe0e695e0f8ed13ca6401a21215
Frederick Pearson <fpearson@cloudstrucc.com>
2025-10-10 16:06:41 -0400

    updated deploy script

--- portal/BuildGCWEB/deploy-power-pages.sh
+++ portal/BuildGCWEB/deploy-power-pages.sh
@@ -806,26 +806,45 @@ update_home_page() {
 
 # Main installation function
 run_portal_template_install() {
+
+#####################################
+# STEP 1: EXTRACT GCWEB FILES
+#####################################
     echo "Extracting theme files..."
-    unzip -o "$ZIP_FILE_PATH" -d "$EXTRACTION_PATH"
-    
+    # unzip -o "$ZIP_FILE_PATH" -d "$EXTRACTION_PATH"
+
+#####################################
+# STEP 2: CREATE SNIPPETS
+#####################################
     echo "Creating snippets..."
     create_snippets
-    
+
+#####################################
+# STEP 3: CREATE TEMPLATES
+#####################################
     echo "Writing templates..."
-    write_templates "$BASE_PATH_TEMPLATES"
-    
+    # write_templates "$BASE_PATH_TEMPLATES"
+
+#####################################
+# STEP 4: UPDATE HOME PAGE COPY
+#####################################
     echo "Updating home page..."
-    update_home_page "$PAGE_TEMPLATE_NAME_NEW_HOME"
+    # update_home_page "$PAGE_TEMPLATE_NAME_NEW_HOME"
     
+#####################################
+# STEP 5: CREATE WEB PAGES & WEB FILES
+#####################################
     echo "Writing hierarchy (using the extracted gcweb zip file structure & files)..."
     >&2 echo "DEBUG: HOME_PAGE_ID before write_hierarchy: $HOME_PAGE_ID"
     >&2 echo "DEBUG: Calling write_hierarchy with path: ${EXTRACTION_PATH}${THEME_ROOT_FOLDER_NAME}"
     
-    write_hierarchy "${EXTRACTION_PATH}${THEME_ROOT_FOLDER_NAME}" "$HOME_PAGE_ID"
-    
+    # write_hierarchy "${EXTRACTION_PATH}${THEME_ROOT_FOLDER_NAME}" "$HOME_PAGE_ID"
+
+#####################################
+# STEP 6: UPSERT THE BASELINE STYLES REQUIRED BY POWER PAGES
+#####################################
     echo "Updating baseline styles..."
-    update_baseline_styles
+    # update_baseline_styles
     
     echo "Portal template installation complete!"
 }
